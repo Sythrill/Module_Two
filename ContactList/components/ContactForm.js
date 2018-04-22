faf0299299bd8b1d8805c61e3449529c9ff364b5
Sythrill <ksciwiarska@gmail.com>
2018-04-22 15:48:57 +0200

    add html5 form validation

--- ContactList/components/ContactForm.js
+++ ContactList/components/ContactForm.js
@@ -1,28 +1,32 @@
 var ContactForm = React.createClass({
-  propTypes: {
-    contact: React.PropTypes.object.isRequired
-  },
+    propTypes: {
+        contact: React.PropTypes.object.isRequired
+    },
 
-  render: function() {
-    return (
-      React.createElement('form', {className: 'contactForm'},
-        React.createElement('input', {
-          type: 'text',
-          placeholder: 'Imię',
-          value: this.props.contact.firstName,
-        }),
-        React.createElement('input', {
-          type: 'text',
-          placeholder: 'Nazwisko',
-          value: this.props.contact.lastName,
-        }),
-        React.createElement('input', {
-          type: 'email',
-          placeholder: 'Email',
-          value: this.props.contact.email,
-        }),
-        React.createElement('button', {type: 'submit'}, "Dodaj kontakt")
-      )
-    )
-  },
+    render: function () {
+        return (
+            React.createElement('form', {className: 'contactForm'},
+                React.createElement('input', {
+                    type: 'text',
+                    placeholder: 'Imię',
+                    pattern: '[a-zA-Z]',
+                    value: this.props.contact.firstName,
+                }),
+                React.createElement('input', {
+                    type: 'text',
+                    required: "required",
+                    pattern: '[a-zA-Z]',
+                    placeholder: 'Nazwisko',
+                    value: this.props.contact.lastName,
+                }),
+                React.createElement('input', {
+                    type: 'email',
+                    required: "required",
+                    placeholder: 'Email',
+                    value: this.props.contact.email,
+                }),
+                React.createElement('button', {type: 'submit'}, "Dodaj kontakt")
+            )
+        )
+    },
 })
